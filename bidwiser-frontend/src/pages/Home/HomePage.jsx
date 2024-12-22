7deee8528ef551b2295ed49b9c2d46b3f3d8c385
Vedant Sanap <vedantsanap123@gmail.com>
2024-12-22 11:38:49 +0530

    Homepage

--- bidwiser-frontend/src/pages/Home/HomePage.jsx
+++ bidwiser-frontend/src/pages/Home/HomePage.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-
+import "./homepage.css"
 const HomePage = () => {
   return (
-    <div>
-      HomePage
+    <div className="auction-page">
+    <div className="overlay">
+      <div className="content">
+        <img
+          src="/public/Assets/logo.jpeg" // Replace with your actual logo path
+          alt="Bid Wars Logo"
+          className="logo"
+        />
+        <h2>Select the mode of auction you want to conduct</h2>
+        <div className="buttons">
+          <button className="btn">Team Auction</button>
+          <button className="btn">Solo Auction</button>
+        </div>
+      </div>
     </div>
+  </div>
   )
 }
 
